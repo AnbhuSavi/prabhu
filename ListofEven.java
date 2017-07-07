8cb16d6479fc5428f8dc4bdbb6644492bb1061f4
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-07 10:23:54 +0530

    Create  ListofEven.java

---
+++ ListofEven.java
@@ -0,0 +1,17 @@
+
+import java.*;
+import java.util.*;
+public class  ListofEven
+{
+public static void main(String args[])
+{
+int i;
+for(i=1;i<=100;i++)
+{
+if(i%2==0)
+{
+System.out.println(i);
+}
+}
+}
+}
